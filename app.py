0eef17ec910c43422058f42c00a633b6ed0a9faa
Vivek M R <vivekmrofficial@gmail.com>
2020-11-30 15:30:33 +0530

    Added pytz module and Fixed timezone issue

--- app.py
+++ app.py
@@ -1,14 +1,17 @@
 from flask import Flask,render_template,request,redirect
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
+from pytz import timezone
+import pytz
 app=Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI']='sqlite:///test.db'
 db=SQLAlchemy(app)
+ind=timezone('Asia/Kolkata')
 
 class Todo(db.Model):
     id=db.Column(db.Integer,primary_key=True)
     content=db.Column(db.String(200),nullable=False)
-    date_created=db.Column(db.DateTime,default=datetime.now().replace(microsecond=0))
+    date_created=db.Column(db.DateTime)
 
     def __repr__(self):
         return '<Task %r>' %self.id
@@ -18,7 +21,7 @@ class Todo(db.Model):
 def index():
     if request.method=='POST':
         task_content=request.form['content']
-        new_task=Todo(content=task_content)
+        new_task=Todo(content=task_content,date_created=datetime.now(ind).replace(microsecond=0))
         try:
             db.session.add(new_task)
             db.session.commit()
